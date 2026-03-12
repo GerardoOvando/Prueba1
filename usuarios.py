7fcb2a04ded49a85603533ce0ef57ff5ad9fc9c1
HenryMcbride <saveusall1984@gmail.com>
2026-03-12 14:01:58 -0600

    feat(backend)Se agrego una clase adicional y se aplico polimorfismo

--- usuarios.py
+++ usuarios.py
@@ -10,10 +10,38 @@ class Users():
 
     def __str__(self):
         return f'User name: {self.user_name}\nEmail: {self.email}\nTel: {self.tel}'
+
+
+    def autenticar(self):
+        return "USuario no autenticado"
     
+
+
 user1 = Users('Gerardo', 'gerardo@gmail.com', '123456789')
+print( user1.autenticar())
 
-def sumar():
-    return 2 + 1
 
-print(sumar())
+
+
+#herencia
+class Admin(Users):
+    __is_admin = bool
+
+    def __init__(self, is_admin, user_name, email, tel):
+        super().__init__(user_name, email, tel)
+
+        self.is_admin = is_admin
+
+    
+    def __str__(self):
+        return super().__str__() +  f'Is admin : {self.is_admin}'
+
+
+    def autenticar(self):
+        return "USuario Autenticado"
+    
+
+userAdmin = Admin(True,'Enrique','enrique@gmail.com','234567890')
+print(userAdmin.autenticar())
+
+
